62d55fc47113ab235c355767247bf641755eeba2
Juan Paredes <juanchuu.jfpc@gmail.com>
2020-11-26 16:45:24 +0100

    first commit

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const cors = require('cors');
+
+//connection to db
+const mongoose = require('mongoose');
+
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//setting up DB connection
+
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('Mongo db working and connected')
+})
+
+
+//listening app
+app.listen(port, () => {
+  console.log(`working as expected :) on port: ${port}`);
+});
